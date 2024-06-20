7cb9f399a4e17d736a707fd64c7e8438449745ce
baebae02 <bae4614@gmail.com>
2024-06-20 16:27:45 +0900

    feat: init

---
+++ src/main/java/com/aloc/aloc/AlocApplication.java
@@ -0,0 +1,13 @@
+package com.aloc.aloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aloc/aloc/AlocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aloc.aloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
