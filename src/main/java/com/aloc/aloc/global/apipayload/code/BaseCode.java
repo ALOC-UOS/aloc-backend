51c3708166f0f4613d1908510f11ae69b02cfd06
baebae02 <bae4614@gmail.com>
2024-06-23 22:57:10 +0900

    feat: add Custom ApiResponse

---
+++ src/main/java/com/aloc/aloc/global/apipayload/code/BaseCode.java
@@ -0,0 +1,9 @@
+package com.aloc.aloc.global.apipayload.code;
+
+public interface BaseCode {
+
+	public ReasonDto getReason();
+
+	public ReasonDto getReasonHttpStatus();
+
+}
---
+++ src/main/java/com/aloc/aloc/global/apipayload/status/SuccessStatus.java
@@ -0,0 +1,39 @@
+package com.aloc.aloc.global.apipayload.status;
+
+import org.springframework.http.HttpStatus;
+
+import com.aloc.aloc.global.apipayload.code.BaseCode;
+import com.aloc.aloc.global.apipayload.code.ReasonDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus implements BaseCode {
+	_OK(HttpStatus.OK, "COMMON200", "성공입니다."),
+	_CREATED(HttpStatus.CREATED, "COMMON201", "생성되었습니다.");
+
+	private final HttpStatus httpStatus;
+	private final String code;
+	private final String message;
+
+	@Override
+	public ReasonDto getReason() {
+		return ReasonDto.builder()
+			.isSuccess(true)
+			.code(code)
+			.message(message)
+			.build();
+	}
+
+	@Override
+	public ReasonDto getReasonHttpStatus() {
+		return ReasonDto.builder()
+			.httpStatus(httpStatus)
+			.isSuccess(true)
+			.code(code)
+			.message(message)
+			.build();
+	}
+}
