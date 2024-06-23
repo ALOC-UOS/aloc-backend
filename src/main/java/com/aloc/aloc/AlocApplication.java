fb1ccfbab9f86871e6c8e14df7feb038c5355aa5
baebae02 <bae4614@gmail.com>
2024-06-23 22:56:33 +0900

    feat: add Enable audity annotation

--- src/main/java/com/aloc/aloc/AlocApplication.java
+++ src/main/java/com/aloc/aloc/AlocApplication.java
@@ -2,8 +2,10 @@ package com.aloc.aloc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AlocApplication {
 
 	public static void main(String[] args) {
