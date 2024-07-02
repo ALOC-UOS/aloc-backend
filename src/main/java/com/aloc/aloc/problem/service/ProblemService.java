6da9594ca297459d525e17897c645daf88ea08c5
baebae02 <bae4614@gmail.com>
2024-07-03 01:12:25 +0900

    feat: make check today problem API

--- src/main/java/com/aloc/aloc/problem/service/ProblemService.java
+++ src/main/java/com/aloc/aloc/problem/service/ProblemService.java
@@ -5,6 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.aloc.aloc.problem.dto.response.ProblemResponseDto;
+import com.aloc.aloc.problem.entity.Problem;
+import com.aloc.aloc.problem.repository.ProblemRepository;
+import com.aloc.aloc.problemtype.enums.Course;
+import com.aloc.aloc.problemtype.enums.Routine;
+import com.aloc.aloc.problemtype.repository.ProblemTypeRepository;
 import com.aloc.aloc.user.dto.response.SolvedUserResponseDto;
 
 import lombok.RequiredArgsConstructor;
@@ -13,6 +18,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProblemService {
 	private final ProblemFacade problemFacade;
+	private final ProblemRepository problemRepository;
+	private final ProblemTypeRepository problemTypeRepository;
 
 	public List<ProblemResponseDto> getProblems() {
 		return problemFacade.getVisibleProblemsWithSolvingCount();
@@ -20,6 +27,17 @@ public class ProblemService {
 
 	public List<SolvedUserResponseDto> getSolvedUserListByProblemId(Long problemId) {
 		return problemFacade.getSolvedUserList(problemId);
+	}
+
+	public ProblemResponseDto findTodayProblemByCourse(Course course) {
+		Long problemTypeId =
+			problemTypeRepository.findProblemTypeByCourseAndRoutine(course, Routine.DAILY).getId();
+		System.out.println(problemTypeId);
+		Problem todayProblem = problemRepository.findLatestPublicProblemByProblemTypeId(problemTypeId);
+		if (todayProblem == null) {
+			throw new IllegalArgumentException("오늘의 문제가 없습니다.");
+		}
+		return problemFacade.mapToProblemResponseDto(todayProblem);
 	}
 
 }
