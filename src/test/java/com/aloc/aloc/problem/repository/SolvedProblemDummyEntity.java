22ab9dde5a8ac4ad3f7373debd1ebd56e8c12121
baebae02 <bae4614@gmail.com>
2024-07-04 00:39:59 +0900

    feat: solvedProblem repository test 추가

---
+++ src/test/java/com/aloc/aloc/problem/repository/SolvedProblemDummyEntity.java
@@ -0,0 +1,56 @@
+package com.aloc.aloc.problem.repository;
+
+import com.aloc.aloc.algorithm.Algorithm;
+import com.aloc.aloc.problem.entity.Problem;
+import com.aloc.aloc.problem.entity.SolvedProblem;
+import com.aloc.aloc.problemtype.ProblemType;
+import com.aloc.aloc.problemtype.enums.Course;
+import com.aloc.aloc.problemtype.enums.Routine;
+import com.aloc.aloc.user.User;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+public class SolvedProblemDummyEntity {
+	User user1;
+	User user2;
+	Problem problem1;
+	Algorithm algorithm1;
+	ProblemType problemType1;
+	SolvedProblem sp1;
+	SolvedProblem sp2;
+	public void setupEntities(TestEntityManager em) {
+		user1 = User.builder()
+			.githubId("githubId1")
+			.baekjoonId("baekjoonId")
+			.username("username1")
+			.password("password")
+			.studentId("studentId")
+			.build();
+		user2 = User.builder()
+			.githubId("githubId2")
+			.baekjoonId("baekjoonId2")
+			.username("username2")
+			.password("password")
+			.studentId("studentId2")
+			.build();
+		em.persist(user1);
+		em.persist(user2);
+
+		algorithm1 =
+			new Algorithm(1, "Algorithm 1", 2, null);
+		em.persist(algorithm1);
+
+		problemType1 =
+			ProblemType.builder().routine(Routine.DAILY).course(Course.FULL).build();
+		em.persist(problemType1);
+
+		problem1 =
+			new Problem("Problem 1", 3, algorithm1, 1, problemType1);
+		em.persist(problem1);
+
+		sp1 = new SolvedProblem(user1, problem1);
+		sp2 = new SolvedProblem(user2, problem1);
+		em.persist(sp1);
+		em.persist(sp2);
+	}
+
+}
---
+++ src/test/java/com/aloc/aloc/problem/repository/SolvedProblemRepositoryTest.java
@@ -0,0 +1,91 @@
+package com.aloc.aloc.problem.repository;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import com.aloc.aloc.algorithm.Algorithm;
+import com.aloc.aloc.problem.entity.Problem;
+import com.aloc.aloc.problem.entity.SolvedProblem;
+import com.aloc.aloc.problemtype.ProblemType;
+import com.aloc.aloc.problemtype.enums.Course;
+import com.aloc.aloc.problemtype.enums.Routine;
+import com.aloc.aloc.user.User;
+import java.util.List;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@DataJpaTest
+@ActiveProfiles("dev")
+@Transactional
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class SolvedProblemRepositoryTest {
+	@Autowired
+	private TestEntityManager em;
+
+	@Autowired
+	private SolvedProblemRepository solvedProblemRepository;
+
+	private SolvedProblemDummyEntity dummyEntity;
+	@BeforeEach
+	void setUp() {
+		dummyEntity = new SolvedProblemDummyEntity();
+		dummyEntity.setupEntities(em);
+	}
+
+	@Test
+	@Tag("Repository")
+	@DisplayName("문제 ID를 통해 해결된 문제 리스트 가져오기")
+	void findAllByProblemId_ShouldReturnSolvedProblemList() {
+		// when
+		List<SolvedProblem> problems = solvedProblemRepository.findAllByProblemId(dummyEntity.problem1.getId());
+
+		// then
+		assertThat(problems).hasSize(2);
+
+		// 푼 사용자 목록을 가져오기 위한 함수, 정렬은 보장되지 않음
+		AssertionsForClassTypes.assertThat(problems.get(0).getId()).isEqualTo(dummyEntity.sp1.getId());
+		AssertionsForClassTypes.assertThat(problems.get(1).getId()).isEqualTo(dummyEntity.sp2.getId());
+	}
+
+	@Test
+	@Tag("Repository")
+	@DisplayName("문제 ID를 통해 해결한 사용자 수 가져오기")
+	void countSolvingUsersByProblemId_ShouldReturnSolvingUsersCount() {
+		// when
+		Integer count = solvedProblemRepository.countSolvingUsersByProblemId(dummyEntity.problem1.getId());
+
+		// then
+		assertThat(count).isEqualTo(2);
+	}
+
+	@Test
+	@Tag("Repository")
+	@DisplayName("사용자 ID와 문제 ID를 통해 해결된 문제가 존재하는지 확인")
+	void existsByUserIdAndProblemId_ShouldReturnTrue() {
+		Boolean exists = solvedProblemRepository.existsByUserIdAndProblemId(
+			dummyEntity.user1.getId(),
+			dummyEntity.problem1.getId()
+		);
+		assertThat(exists).isTrue();
+	}
+
+	@Test
+	@Tag("Repository")
+	@DisplayName("사용자 ID와 문제 ID를 통해 해결된 문제가 존재하지 않는지 확인")
+	void existsByUserIdAndProblemId_ShouldReturnFalse() {
+		Boolean exists = solvedProblemRepository.existsByUserIdAndProblemId(
+			dummyEntity.user1.getId(),
+			100L
+		);
+		assertThat(exists).isFalse();
+	}
+
+}
