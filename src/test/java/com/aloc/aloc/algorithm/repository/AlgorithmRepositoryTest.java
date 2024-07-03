f3ac0b3fe16c232e0d86983ac2cc79085391d518
baebae02 <bae4614@gmail.com>
2024-07-04 00:05:13 +0900

    fix: profile dev일 때만 test가 돌아가도록 설정

--- src/test/java/com/aloc/aloc/algorithm/repository/AlgorithmRepositoryTest.java
+++ src/test/java/com/aloc/aloc/algorithm/repository/AlgorithmRepositoryTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.aloc.aloc.algorithm.Algorithm;
@@ -21,6 +22,7 @@ import jakarta.persistence.EntityManager;
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("dev")
 class AlgorithmRepositoryTest {
 
 	@Autowired
