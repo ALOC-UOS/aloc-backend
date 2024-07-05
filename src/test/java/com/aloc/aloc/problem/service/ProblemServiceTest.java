e20b98630a9a27bca1661d2f49fb621c30a0b5c6
baebae02 <bae4614@gmail.com>
2024-07-05 23:45:51 +0900

    feat: remove unused import statement

--- src/test/java/com/aloc/aloc/problem/service/ProblemServiceTest.java
+++ src/test/java/com/aloc/aloc/problem/service/ProblemServiceTest.java
@@ -7,7 +7,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
