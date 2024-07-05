3e18cd5c14642e6a131edfdbb0b0079461f44685
baebae02 <bae4614@gmail.com>
2024-07-05 23:45:15 +0900

    feat: add profile number & rank when sign-up

--- src/main/java/com/aloc/aloc/auth/service/AuthService.java
+++ src/main/java/com/aloc/aloc/auth/service/AuthService.java
@@ -4,6 +4,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.aloc.aloc.auth.dto.UserLoginRequestDto;
+import com.aloc.aloc.scraper.BaekjoonRankScrapingService;
+import com.aloc.aloc.scraper.GithubProfileScrapingService;
 import com.aloc.aloc.user.dto.request.UserRequestDto;
 import com.aloc.aloc.user.repository.UserRepository;
 
@@ -14,17 +16,31 @@ import lombok.RequiredArgsConstructor;
 public class AuthService {
 	private final UserRepository userRepository;
 	private final BCryptPasswordEncoder passwordEncoder;
+	private final GithubProfileScrapingService githubProfileScrapingService;
+	private final BaekjoonRankScrapingService baekjoonRankScrapingService;
 
 	public void login(UserLoginRequestDto userLoginRequestDto) {
 		System.out.println("Login request received for username: " + userLoginRequestDto.getGithubId());
 	}
 
 	public void signUp(UserRequestDto userRequestDto) {
+		// 이미 존재하는 유저인지 확인
 		if (userRepository.existsByGithubId(userRequestDto.getGithubId())
 			|| userRepository.existsByBaekjoonId(userRequestDto.getBaekjoonId())) {
 			throw new IllegalArgumentException("이미 존재하는 유저입니다.");
 		}
-		userRepository.save(userRequestDto.toEntity(passwordEncoder));
+
+		// 깃허브 프로필 아이디 가져오기
+		String githubProfileNumber = githubProfileScrapingService
+			.extractProfileNumber(userRequestDto.getGithubId());
+		if (githubProfileNumber == null) {
+			throw new IllegalArgumentException("깃허브 프로필을 찾을 수 없습니다.");
+		}
+
+		// 백준 랭킹 가져오기
+		Integer rank = baekjoonRankScrapingService.extractBaekjoonRank(userRequestDto.getBaekjoonId());
+		System.out.println(rank);
+		userRepository.save(userRequestDto.toEntity(githubProfileNumber, rank, passwordEncoder));
 	}
 
 }
