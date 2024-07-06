6f8a9f45b6fc051be4a6f7372edb2444ecb34c6b
조수아 <suacho0724@gmail.com>
2024-07-06 17:03:13 +0900

    feat: rebase main

--- src/main/java/com/aloc/aloc/algorithm/repository/AlgorithmRepository.java
+++ src/main/java/com/aloc/aloc/algorithm/repository/AlgorithmRepository.java
@@ -3,7 +3,6 @@ package com.aloc.aloc.algorithm.repository;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
