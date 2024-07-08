4fc28f6d03e2111f2eab16b01491d66e1e5e2bf8
baebae02 <bae4614@gmail.com>
2024-07-08 22:12:04 +0900

    feat: add problem facade test

--- src/test/java/com/aloc/aloc/problem/service/ProblemFacadeTest.java
+++ src/test/java/com/aloc/aloc/problem/service/ProblemFacadeTest.java
@@ -1,15 +1,10 @@
 package com.aloc.aloc.problem.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -28,13 +23,13 @@ import com.aloc.aloc.problem.repository.SolvedProblemRepository;
 import com.aloc.aloc.user.User;
 import com.aloc.aloc.user.dto.response.SolvedUserResponseDto;
 
+
 @ExtendWith(MockitoExtension.class)
-public class ProblemServiceTest {
+public class ProblemFacadeTest {
+	@InjectMocks
+	private ProblemFacade problemFacade;
 
 	@Mock
-	private ProblemSolvingService problemSolvingService;
-
-	@InjectMocks
 	private ProblemService problemService;
 
 	@Mock
@@ -42,6 +37,9 @@ public class ProblemServiceTest {
 
 	@Mock
 	private SolvedProblemRepository solvedProblemRepository;
+
+	@Mock
+	private ProblemSolvingService problemSolvingService;
 
 	@Mock
 	private ProblemMapper problemMapper;
@@ -114,38 +112,26 @@ public class ProblemServiceTest {
 	}
 
 	@Test
-	@DisplayName("문제 목록 조회 성공 테스트")
-	void getProblems_shouldReturnListOfProblemResponseDto() {
-		// given
-		Problem p1 = problems.get(0);
-		Problem p2 = problems.get(1);
-		ProblemResponseDto dto1 = problemResponseDtos.get(0);
-		ProblemResponseDto dto2 = problemResponseDtos.get(1);
-		when(problemRepository.findAllByHiddenIsNullOrderByCreatedAtDesc()).thenReturn(problems);
-		when(problemMapper.mapToProblemResponseDto(p1)).thenReturn(dto1);
-		when(problemMapper.mapToProblemResponseDto(p2)).thenReturn(dto2);
-
-		// When
-		List<ProblemResponseDto> result = problemService.getVisibleProblemsWithSolvingCount();
-
-		// Then
-		assertEquals(2, result.size());
-		assertEquals("Problem 1", result.get(0).getTitle());
-		assertEquals("Problem 2", result.get(1).getTitle());
-
-		verify(problemRepository).findAllByHiddenIsNullOrderByCreatedAtDesc();
-		verify(problemMapper, times(2)).mapToProblemResponseDto(any(Problem.class));
-
-	}
-
-	@Test
-	@DisplayName("문제 목록 조회 실패 테스트")
-	void getSolvedUserListByInvalidProblemId_shouldThrowIllegalArgumentException() {
+	@DisplayName("문제 푼 유저 목록 조회 테스트")
+	void getSolvedUserListByProblemId_shouldReturnListOfSolvedUserResponseDto() {
 		// Given
 		Long problemId = 1L;
+		List<SolvedUserResponseDto> expectedUsers = solvedUsers;
+		when(problemSolvingService.getSolvedUserListByProblemId(problemId)).thenReturn(solvedProblems);
+		System.out.println(solvedProblems.size());
+
+		// problemMapper.mapToSolvedUserResponseDto() 모킹 추가
+		for (int i = 0; i < solvedProblems.size(); i++) {
+			when(problemMapper.mapToSolvedUserResponseDto(solvedProblems.get(i).getUser(), solvedProblems.get(i)))
+				.thenReturn(expectedUsers.get(i));
+			System.out.println(solvedProblems.get(i).getUser().getUsername());
+		}
+
 		// When
-		when(problemRepository.findById(problemId)).thenReturn(Optional.empty());
+		List<SolvedUserResponseDto> actualUsers = problemFacade.getSolvedUserListByProblemId(problemId);
+
 		// Then
-		assertThrows(IllegalArgumentException.class, () -> problemService.checkProblemExist(problemId));
+		assertEquals(expectedUsers, actualUsers);
 	}
+
 }
