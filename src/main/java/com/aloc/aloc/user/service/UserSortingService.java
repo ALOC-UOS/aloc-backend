6b28c160fc1cd0c00f6b8b5722c5a2b2fae5aaa8
baebae02 <bae4614@gmail.com>
2024-07-20 20:09:20 +0900

    fix: user sort logic에 오늘의 문제 풀이 여부 추가

--- src/main/java/com/aloc/aloc/user/service/UserSortingService.java
+++ src/main/java/com/aloc/aloc/user/service/UserSortingService.java
@@ -7,8 +7,12 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.aloc.aloc.problem.entity.Problem;
 import com.aloc.aloc.problem.entity.UserProblem;
+import com.aloc.aloc.problem.repository.ProblemRepository;
 import com.aloc.aloc.problem.repository.UserProblemRepository;
+import com.aloc.aloc.problemtype.enums.Routine;
+import com.aloc.aloc.problemtype.repository.ProblemTypeRepository;
 import com.aloc.aloc.user.User;
 
 import lombok.AllArgsConstructor;
@@ -18,12 +22,33 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class UserSortingService {
 	private final UserProblemRepository userProblemRepository;
+	private final ProblemTypeRepository problemTypeRepository;
+	private final ProblemRepository problemRepository;
 
 	public List<User> sortUserList(List<User> userList) {
 		return userList.stream()
-			.sorted(Comparator.comparing(this::sortByRank)
+			.sorted(Comparator
+				.comparing(this::hasSolvedToday).reversed() // 오늘 문제 푼 사람 먼저
+				.thenComparing(this::sortByRank) // 랭크가 높은 사람 먼저
+				// 최근에 푼 사람 먼저
 				.thenComparing(this::getLatestSolvedTime, Comparator.nullsLast(Comparator.reverseOrder())))
 			.collect(Collectors.toList());
+	}
+
+	private boolean hasSolvedToday(User user) {
+		// 유저의 오늘의 문제 타입을 가져옵니다.
+		Long problemTypeId =
+			problemTypeRepository.findProblemTypeByCourseAndRoutine(user.getCourse(), Routine.DAILY)
+				.orElseThrow(() -> new IllegalArgumentException("오늘의 문제 타입이 없습니다."))
+				.getId();
+
+		// 오늘의 문제를 가져옵니다.
+		Problem todayProblem = problemRepository.findLatestPublicProblemByProblemTypeId(problemTypeId);
+
+		// 오늘의 문제 풀이 여부를 가져옵니다.
+		return userProblemRepository.findByUserIdAndProblemId(user.getId(), todayProblem.getId())
+			.map(UserProblem::getIsSolved)
+			.orElse(false);
 	}
 
 	private Pair<Integer, Integer> sortByRank(User user) {
@@ -37,14 +62,8 @@ public class UserSortingService {
 			.orElse(null);
 	}
 
-	private static class Pair<T extends Comparable<T>, U extends Comparable<U>> implements Comparable<Pair<T, U>> {
-		private final T first;
-		private final U second;
-
-		public Pair(T first, U second) {
-			this.first = first;
-			this.second = second;
-		}
+	private record Pair<T extends Comparable<T>, U extends Comparable<U>>(T first, U second) implements
+		Comparable<Pair<T, U>> {
 
 		@Override
 		public int compareTo(Pair<T, U> other) {
