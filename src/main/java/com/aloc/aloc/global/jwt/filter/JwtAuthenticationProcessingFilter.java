2e074913754bd466be1a93ffb5f90cb95a17064e
baebae02 <bae4614@gmail.com>
2024-07-22 15:25:41 +0900

    refactor: rollback jwt processing filter

--- src/main/java/com/aloc/aloc/global/jwt/filter/JwtAuthenticationProcessingFilter.java
+++ src/main/java/com/aloc/aloc/global/jwt/filter/JwtAuthenticationProcessingFilter.java
@@ -33,60 +33,50 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
 
 	private final GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
 
+	/**
+	 * 1. 리프레시 토큰이 오는 경우 -> 유효하면 AccessToken 헤더에 재발급후, 필터 진행 X, 바로 튕기기, 200과 함께 리턴
+	 *
+	 * 2. 리프레시 토큰은 없고 AccessToken만 있는 경우 -> 유저정보 저장후 필터 계속 진행
+	 */
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 		throws ServletException, IOException, java.io.IOException {
 		String noCheckUrl = "/api2/login";
-		String refreshToken = jwtService.extractRefreshToken(request).orElse(null);
-		String accessToken = jwtService.extractAccessToken(request).orElse(null);
-
-		// 로그인 시도
 		if (request.getRequestURI().equals(noCheckUrl)) {
 			filterChain.doFilter(request, response);
 			return;
 		}
 
-		// accessToken이 있고 유효한 경우
-		if (processAccessToken(accessToken)) {
-			filterChain.doFilter(request, response);
+		System.out.println(request.getHeader("Authorization-refresh"));
+		// 원래는 refreshToken 안담아서 보냄. 재발급 필요할 때만 보내는 것이 맞음
+		String refreshToken = jwtService
+			.extractRefreshToken(request)
+			.filter(jwtService::isTokenValid)
+			.orElse(null);
+
+		System.out.println(refreshToken);
+		// 리프레시 토큰이 있고 & 유효한 경우 -> AccessToken 재발급 후 필터 진행 X
+		if (refreshToken != null) {
+			checkRefreshTokenAndReIssueAccessToken(response, refreshToken);
 			return;
 		}
 
-		// refreshToken이 있고 유효한 경우
-		processRefreshToken(response, refreshToken);
+		// 리프레시 토큰이 없거나 유효하지 않은 경우 -> AccessToken 유효성 검사 후 필터 진행
+		checkAccessTokenAndAuthentication(request, response, filterChain);
+	}
+
+	private void checkAccessTokenAndAuthentication(
+		HttpServletRequest request,
+		HttpServletResponse response,
+		FilterChain filterChain
+	)
+		throws ServletException, IOException, java.io.IOException {
+		jwtService.extractAccessToken(request).filter(jwtService::isTokenValid)
+			.flatMap(accessToken -> jwtService.extractGithubId(accessToken)
+				.flatMap(userRepository::findByGithubId)).ifPresent(this::saveAuthentication);
+
 		filterChain.doFilter(request, response);
 	}
-
-	private boolean processAccessToken(String accessToken) {
-		if (accessToken != null && jwtService.isTokenValid(accessToken)) {
-			setAuthentication(accessToken);
-			return true;
-		}
-		return false;
-	}
-
-	private void processRefreshToken(HttpServletResponse response, String refreshToken) {
-		if (refreshToken != null && jwtService.isTokenValid(refreshToken)) {
-			handleValidRefreshToken(response, refreshToken);
-		}
-	}
-
-	private void handleValidRefreshToken(HttpServletResponse response, String refreshToken) {
-		String githubId = getGithubIdByRefreshToken(refreshToken).orElse(null);
-		if (githubId != null) {
-			String newAccessToken = jwtService.createAccessToken(githubId);
-			jwtService.setAccessTokenHeader(response, newAccessToken);
-//			response.setHeader("Authorization", "Bearer " + newAccessToken);
-			setAuthentication(newAccessToken);
-		}
-	}
-
-	private void setAuthentication(String accessToken) {
-		jwtService.extractGithubId(accessToken)
-			.flatMap(userRepository::findByGithubId)
-			.ifPresent(this::saveAuthentication);
-	}
-
 
 	private void saveAuthentication(User user) {
 		GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(
@@ -111,8 +101,9 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
 		SecurityContextHolder.setContext(context);
 	}
 
-	private Optional<String> getGithubIdByRefreshToken(String refreshToken) {
-		return userRepository.findByRefreshToken(refreshToken)
-			.map(User::getGithubId);
+	private void checkRefreshTokenAndReIssueAccessToken(HttpServletResponse response, String refreshToken) {
+		userRepository.findByRefreshToken(refreshToken).ifPresent(
+			user -> jwtService.sendAccessToken(response, jwtService.createAccessToken(user.getGithubId()))
+		);
 	}
 }
