8c0919af7769156ee724f01d3d4bbdb3ee713f6a
조수아 <suacho0724@gmail.com>
2024-07-29 01:56:37 +0900

    fix: 스케쥴 시간 수정

--- src/main/java/com/aloc/aloc/scheduler/ProblemCrawlingScheduler.java
+++ src/main/java/com/aloc/aloc/scheduler/ProblemCrawlingScheduler.java
@@ -15,7 +15,7 @@ public class ProblemCrawlingScheduler {
 	private final ProblemScrapingService problemScraperService;
 	private final DiscordWebhookService discordWebhookService;
 
-	@Scheduled(cron = "0 */3 * * * MON")
+	@Scheduled(cron = "0 0 0 * * MON")
 	// 매주 월요일에 문제를 크롤링해옵니다.
 	public void scheduleAddProblemsForThisWeek() {
 		try {
