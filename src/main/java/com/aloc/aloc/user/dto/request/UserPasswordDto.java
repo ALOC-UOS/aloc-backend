fcbb8c0fbd8fa1d732da161e1193c862a7bd9ca3
조수아 <suacho0724@gmail.com>
2024-07-30 22:25:40 +0900

    🩹 Add dto detail

--- src/main/java/com/aloc/aloc/user/dto/request/UserPasswordDto.java
+++ src/main/java/com/aloc/aloc/user/dto/request/UserPasswordDto.java
@@ -1,10 +1,12 @@
 package com.aloc.aloc.user.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UserPasswordDto {
+	@NotNull
 	@Schema(description = "유저의 현재 비밀 번호", example = "password")
 	private String password;
 }
