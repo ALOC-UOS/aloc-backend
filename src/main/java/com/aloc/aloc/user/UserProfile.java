0aeccb58cde0cf20fae42f08919c7248d98794e1
baebae02 <bae4614@gmail.com>
2024-08-16 18:09:11 +0900

    feat: add userprofile table

---
+++ src/main/java/com/aloc/aloc/user/UserProfile.java
@@ -0,0 +1,23 @@
+package com.aloc.aloc.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserProfile {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+}
