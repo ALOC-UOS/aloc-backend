e6807634bf5ffd60b66cd1e0f93e93b95e5b3d57
baebae02 <bae4614@gmail.com>
2024-09-16 14:25:14 +0900

    🐛 Flyway h2 error

--- src/main/resources/db/migration/V11__Add_userprofile_imagefilename.sql
+++ src/main/resources/db/migration/V11__Add_userprofile_imagefilename.sql
@@ -1,11 +1,1 @@
-DO $$
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1
-        FROM information_schema.columns
-        WHERE table_name = 'user_profile'
-        AND column_name = 'profile_image_file_name'
-    ) THEN
-ALTER TABLE user_profile ADD COLUMN profile_image_file_name VARCHAR(255);
-END IF;
-END $$;
+ALTER TABLE user_profile ADD COLUMN IF NOT EXISTS profile_image_file_name VARCHAR(255);
